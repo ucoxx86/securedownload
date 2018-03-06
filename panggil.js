5184cca6f9a61f06fe9d485963d50180b88ce92e
ucoxx86 <35983385+ucoxx86@users.noreply.github.com>
2018-03-06 11:33:01 +0700

    Update panggil.js

--- panggil.js
+++ panggil.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*3)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2018/03/link1.html"}
 if(x=="2"){xxx="/2018/03/link1.html"}
-if(x=="3"){xxx="/2018/03/link1.html"}a_to_ve[i].href="https://s152.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="/2018/03/link1.html"}a_to_ve[i].href="https://s152.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
